71e617948a8c74eb9e9e1bbd2b56a65d4c25fdec
p9sss <50791329+p9sss@users.noreply.github.com>
2022-10-10 20:29:54 +0800

    pull test2

--- src/main/java/com/service.java
+++ src/main/java/com/service.java
@@ -10,5 +10,6 @@ public class service {
         System.out.println("hot-fix");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("再来个pull试试");
     }
 }
